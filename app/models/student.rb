137843650c4ba2574dee41a02ae0580702f2b24a
slague <stephanielague@gmail.com>
2017-04-06 22:28:06 -0600

    Target-behavior methods and views styling

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,4 +5,23 @@ class Student < ActiveRecord::Base
   has_many :observations
   has_many :behaviors, through: :observations
   has_many :student_target_behaviors
+
+
+
+  def target_behaviors
+    student_target_behaviors.map { |tb| Behavior.find(tb.behavior_id).keyword }
+  end
+
+
+
+
+  def target_behavior_descriptions
+    student_target_behaviors.map { |tb| tb.student_specific_description }
+  end
+
+
+  def target_behavior_descriptions_hash
+    target_behaviors.zip(target_behavior_descriptions).to_h
+  end
+
 end
